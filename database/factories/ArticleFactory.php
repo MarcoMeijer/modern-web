3b8d7b115b0f2f53692c6f4757a6d85e1e7a55b2
Marco Meijer <marcomeijerm@gmail.com>
2022-11-10 10:07:17 +0100

    feat: added posts model

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->sentance(),
+            'body' => fake()->realText(),
+            'published_at' => fake()->dateTimeBetween('-2 weeks', 'now'),
+            'author_id' => fake()->numberBetween(1,5),
+        ];
+    }
+}
