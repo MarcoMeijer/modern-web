207290362ac74913f8fc6088af889533ac4d4e49
Marco Meijer <marcomeijerm@gmail.com>
2022-11-11 09:24:41 +0100

    fix: renamed article to post

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class ArticleFactory extends Factory
+class PostFactory extends Factory
 {
     /**
      * Define the model's default state.
