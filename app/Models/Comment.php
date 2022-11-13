3a8d2d1ea5006e65b2059acb3275c6bf81595c1e
Marco Meijer <marcomeijerm@gmail.com>
2022-11-13 20:47:22 +0100

    feat: model relations

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    public function author()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
 }
