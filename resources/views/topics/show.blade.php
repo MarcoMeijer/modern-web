fb1a6044edc5aa763873f6cf39c8a8fe741b2219
Marco Meijer <marcomeijerm@gmail.com>
2022-11-13 20:47:37 +0100

    feat: topic view showing list of posts

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -1,3 +1,12 @@
 <x-site-layout title="Topic {{$topic->name}}">
 
+    <ul class="list-disc pl-5">
+        @foreach($topic->posts as $post)
+        <li>
+            <a href="{{route('posts.show', $post->id)}}">
+                <span class="font-semibold">{{$post->title}}</span>
+            </a>
+        </li>
+        @endforeach
+    </ul>
 </x-site-layout>
