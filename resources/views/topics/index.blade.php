9972ee0e0c3e38d894b10a3e48c2d14856089f89
Marco Meijer <marcomeijerm@gmail.com>
2022-11-14 11:56:16 +0100

    feat: added profile picture to topics index

--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -3,24 +3,32 @@
     <ul class="flex-col">
         @foreach($topics as $topic)
         <li class="flex-col flex">
-            <a
+            <div
                 class="flex items-stretch flex-row bg-slate-100 border-l border-r border-b border-solid border-neutral-300 px-2 py-1 {{ $loop->index == 0 ? 'border-t' : ''}}"
-                href="{{route('topics.show', $topic->id)}}"
             >
-                <div class="flex flex-initial flex-col justify-center">
-                    <span class="flex font-semibold text-slate-800">{{$topic->name}}</span>
+                <div class="flex flex-col justify-center">
+                    <a href="{{route('topics.show', $topic->id)}}">
+                        <span class="flex font-semibold text-slate-800">{{$topic->name}}</span>
+                    </a>
                     <span class="flex font-serif text-xs text-slate-500">{{$topic->description}}</span>
                 </div>
                 <div class="flex-1"></div>
-                <div class="flex flex-initial flex-col justify-center border-l border-neutral-300 px-2 w-64">
-                    <span class="flex flex-initial font-serif text-sm text-slate-500">{{Str::limit($topic->posts[0]->title, 30)}}</span>
-                    <span class="flex flex-initial font-serif text-xs text-slate-400">{{$topic->posts[0]->comments[0]->published_at}}</span>
+                <div class="flex flex-row items-center border-l border-neutral-300 px-2 w-72">
+                    <img class="flex h-8 w-8 rounded"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt="">
+                    <div class="flex flex-col mx-1">
+                        <a href="{{route('posts.show', $topic->posts[0]->id)}}">
+                            <span class="flex font-serif text-sm text-slate-500">{{Str::limit($topic->posts[0]->title, 30)}}</span>
+                        </a>
+                        <span class="flex font-serif text-xs text-slate-400">{{$topic->posts[0]->comments[0]->published_at}}</span>
+                    </div>
                 </div>
-                <div class="flex flex-initial flex-col justify-center border-l border-neutral-300 px-2 w-24">
-                    <span class="flex flex-initial font-serif text-xs text-slate-500">Posts: {{count($topic->posts)}}</span>
-                    <span class="flex flex-initial font-serif text-xs text-slate-500">Comments: {{count($topic->comments)}}</span>
+                <div class="flex flex-col justify-center border-l border-neutral-300 px-2 w-24">
+                    <span class="flex font-serif text-xs text-slate-500">Posts: {{count($topic->posts)}}</span>
+                    <span class="flex font-serif text-xs text-slate-500">Comments: {{count($topic->comments)}}</span>
                 </div>
-            </a>
+            </div>
         </li>
         @endforeach
     </ul>
