2687702f15bac4f715aab7c29b0e97c739381263
Marco Meijer <marcomeijerm@gmail.com>
2022-11-14 09:39:30 +0100

    feat: improved topic and post layout

--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -1,9 +1,12 @@
 <x-site-layout title="List of topics on this forum">
 
-    <ul class="list-disc pl-5">
+    <ul class="flex-col">
         @foreach($topics as $topic)
-        <li>
-            <a href="{{route('topics.show', $topic->id)}}">
+        <li class="flex-col flex">
+            <a
+                class="flex-row bg-slate-100 border-l border-r border-b border-solid border-neutral-300 p-2 {{ $loop->index == 0 ? 'border-t' : ''}}"
+                href="{{route('topics.show', $topic->id)}}"
+            >
                 <span class="font-semibold">{{$topic->name}}</span>
             </a>
         </li>
--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -1,9 +1,12 @@
 <x-site-layout title="Topic {{$topic->name}}">
 
-    <ul class="list-disc pl-5">
+    <ul class="flex-col">
         @foreach($topic->posts as $post)
-        <li>
-            <a href="{{route('posts.show', $post->id)}}">
+        <li class="flex-col flex">
+            <a
+                class="flex-row bg-slate-100 border-l border-r border-b border-solid border-neutral-300 p-2 {{ $loop->index == 0 ? 'border-t' : ''}}"
+                href="{{route('posts.show', $post->id)}}"
+            >
                 <span class="font-semibold">{{$post->title}}</span>
             </a>
         </li>
