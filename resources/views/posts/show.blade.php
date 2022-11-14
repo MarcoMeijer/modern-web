991d0d70802b8d67fa37e981b3aa18f4cad0a6e7
Marco Meijer <marcomeijerm@gmail.com>
2022-11-14 09:10:46 +0100

    feat: post showing comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,3 +1,10 @@
 <x-site-layout title="Read {{$post->title}}">
 
+    <ul class="list-disc pl-5">
+        @foreach($post->comments as $comment)
+        <li>
+            <span class="font-semibold">{{$comment->body}}</span>
+        </li>
+        @endforeach
+    </ul>
 </x-site-layout>
