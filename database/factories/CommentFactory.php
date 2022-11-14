2e3991485ef9e127002fecaf7ef5d95f4a6bcbb3
Marco Meijer <marcomeijerm@gmail.com>
2022-11-14 09:43:15 +0100

    fix: increased number of posts/comments in seeders

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -17,7 +17,7 @@ class CommentFactory extends Factory
             'body' => $this->faker->realText(),
             'published_at' => $this->faker->dateTimeBetween('-2 weeks', 'now'),
             'author_id' => $this->faker->numberBetween(1, 10),
-            'post_id' => $this->faker->numberBetween(1, 10),
+            'post_id' => $this->faker->numberBetween(1, 50),
         ];
     }
 }
