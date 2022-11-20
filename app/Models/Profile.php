1abbe3e3cea08d97caa14914dadca24816091641
Marco Meijer <marcomeijerm@gmail.com>
2022-11-20 17:57:25 +0100

    feat: added media to profile

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -4,15 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Profile extends Model
+class Profile extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
 
     protected $guarded = [];
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this
+            ->addMediaConversion('thumbnail')
+            ->fit(Manipulations::FIT_CROP, 50, 50)
+            ->nonQueued();
+        $this
+            ->addMediaConversion('big')
+            ->fit(Manipulations::FIT_CROP, 200, 200)
+            ->nonQueued();
     }
 }
