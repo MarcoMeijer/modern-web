cfaebc664c0851eea6b170b37b7b61a764ebee88
Marco Meijer <marcomeijerm@gmail.com>
2022-11-21 12:23:52 +0100

    fix: null profile image not crashing profile badge

--- resources/views/components/profile-badge.blade.php
+++ resources/views/components/profile-badge.blade.php
@@ -1,7 +1,7 @@
 @props(['user'])
 <div class="flex flex-col w-44 items-center">
     <a class="flex flex-col items-center" href="{{route('profile.show', $user->id)}}">
-        <img class="shadow-md h-32 w-32 rounded border-2 border-white" src="{{$user->profile->media->first()->getUrl('big')}}" alt="">
+        <img class="shadow-md h-32 w-32 rounded border-2 border-white" src="{{$user->profile->media->first()?->getUrl('big')}}" alt="">
         <span class="font-semibold">{{$user->username}}</span>
     </a>
     <span class="text-xs text-slate-400 p-1 text-center border-t border-slate-100">Member since <br /> {{$user->email_verified_at}}</span>
