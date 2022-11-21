1e048cf4d95a6967468f1b6d9772399b407d0215
Marco Meijer <marcomeijerm@gmail.com>
2022-11-21 11:16:37 +0100

    feat: profile link in user dropdown

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -37,8 +37,12 @@
                     </x-slot>
 
                     <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
+                        <x-dropdown-link :href="route('profile.show', Auth::user()->id)">
                             {{ __('Profile') }}
+                        </x-dropdown-link>
+
+                        <x-dropdown-link :href="route('profile.edit')">
+                            {{ __('Settings') }}
                         </x-dropdown-link>
 
                         <!-- Authentication -->
