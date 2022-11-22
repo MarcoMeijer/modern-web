e5d2e770b003eab7df8a80b15448ad50e3fe61c8
Marco Meijer <marcomeijerm@gmail.com>
2022-11-22 10:20:10 +0100

    fix: removed dashboard

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,6 @@ Route::resource('messages', App\Http\Controllers\MessagesController::class);
 require __DIR__ . '/auth.php';
 
 // LOGGED IN ROUTES =======================================================
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
 Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
 Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
