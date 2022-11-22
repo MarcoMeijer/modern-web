10bbfe3ba3b8ace55d009d02e1007c98027309d9
Marco Meijer <marcomeijerm@gmail.com>
2022-11-22 14:51:17 +0100

    style: updated forum colors

--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -3,28 +3,28 @@
     <x-white-box class="flex-col p-6">
         @foreach($topics as $topic)
         <div class="flex-col flex">
-            <div class="flex items-stretch flex-row bg-white border-l border-r border-b border-solid border-neutral-300 px-2 py-1 {{ $loop->index == 0 ? 'border-t' : ''}}">
+            <div class="flex items-stretch flex-row bg-white border-l border-r border-b border-solid border-slate-200 px-2 py-1 {{ $loop->index == 0 ? 'border-t' : ''}}">
                 <div class="flex flex-col justify-center">
                     <a href="{{route('topics.threads.index', $topic->id)}}">
-                        <span class="flex font-semibold text-slate-800">{{$topic->name}}</span>
+                        <span class="flex font-semibold text-slate-700">{{$topic->name}}</span>
                     </a>
-                    <span class="flex font-serif text-xs text-slate-700">{{$topic->description}}</span>
+                    <span class="flex font-serif text-xs text-slate-600">{{$topic->description}}</span>
                 </div>
                 <div class="flex-1"></div>
-                <div class="flex flex-row items-center border-l border-neutral-300 px-2 w-72">
+                <div class="flex flex-row items-center border-l border-slate-200 px-2 w-72">
                     <img class="flex h-8 w-8 rounded" src="{{$topic->threads[0]->messages[0]->author->profile->getImageUrl('thumbnail')}}" alt="">
                     <div class="flex flex-col mx-1">
                         <a href="{{route('threads.show', $topic->threads[0]->id)}}">
-                            <span class="flex font-serif text-sm text-slate-700">{{Str::limit($topic->threads[0]->title, 30)}}</span>
+                            <span class="flex font-serif text-sm text-slate-600">{{Str::limit($topic->threads[0]->title, 30)}}</span>
                         </a>
                         @if(count($topic->threads[0]->messages) > 0)
-                        <span class="flex font-serif text-xs text-slate-600">{{$topic->threads[0]->messages[0]->published_at}}</span>
+                        <span class="flex font-serif text-xs text-slate-500">{{$topic->threads[0]->messages[0]->published_at}}</span>
                         @endif
                     </div>
                 </div>
-                <div class="flex flex-col justify-center border-l border-neutral-300 px-2 w-24">
-                    <span class="flex font-serif text-xs text-slate-700">Threads: {{count($topic->threads)}}</span>
-                    <span class="flex font-serif text-xs text-slate-700">Messages: {{count($topic->messages)}}</span>
+                <div class="flex flex-col justify-center border-l border-slate-200 px-2 w-24">
+                    <span class="flex font-serif text-xs text-slate-600">Threads: {{count($topic->threads)}}</span>
+                    <span class="flex font-serif text-xs text-slate-600">Messages: {{count($topic->messages)}}</span>
                 </div>
             </div>
         </div>
--- resources/views/topics/threads/index.blade.php
+++ resources/views/topics/threads/index.blade.php
@@ -3,21 +3,21 @@
     <x-white-box class="flex-col p-6">
         @foreach($topic->threads as $thread)
         <div class="flex-col flex">
-            <div class="flex items-stretch flex-row bg-white border-l border-r border-b border-solid border-neutral-300 px-2 py-1 {{ $loop->index == 0 ? 'border-t' : ''}}">
+            <div class="flex items-stretch flex-row bg-white border-l border-r border-b border-solid border-slate-200 px-2 py-1 {{ $loop->index == 0 ? 'border-t' : ''}}">
                 <div class="flex flex-row items-center">
                     <img class="flex h-8 w-8 rounded" src="{{$thread->messages[0]->author->profile->getImageUrl('thumbnail')}}" alt="">
                     <div class="flex flex-col mx-1">
                         <a href="{{route('threads.show', $thread->id)}}">
-                            <span class="flex font-semibold text-sm text-slate-800">{{Str::limit($thread->title, 70)}}</span>
+                            <span class="flex font-semibold text-sm text-slate-700">{{Str::limit($thread->title, 70)}}</span>
                         </a>
                         @if(count($thread->messages) > 0)
-                        <span class="flex font-serif text-xs text-slate-700">{{$thread->messages[0]->published_at}}</span>
+                        <span class="flex font-serif text-xs text-slate-600">{{$thread->messages[0]->published_at}}</span>
                         @endif
                     </div>
                 </div>
                 <div class="flex-1"></div>
-                <div class="flex flex-col justify-center border-l border-neutral-300 px-2 w-24">
-                    <span class="flex font-serif text-xs text-slate-700">Replies: {{count($thread->messages) - 1}}</span>
+                <div class="flex flex-col justify-center border-l border-slate-200 px-2 w-24">
+                    <span class="flex font-serif text-xs text-slate-600">Replies: {{count($thread->messages) - 1}}</span>
                 </div>
             </div>
         </div>
