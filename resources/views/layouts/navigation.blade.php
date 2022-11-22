ba74b2953a1cfba08f35812bd3a1aa55da7c37e9
Marco Meijer <marcomeijerm@gmail.com>
2022-11-22 16:12:54 +0100

    feat: added icons

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                    <x-application-logo class="block w-8 h-8 fill-current text-gray-800" />
                 </div>
 
                 <!-- Navigation Links -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,10 @@
     <x-white-box class="p-6 flex-col space-y-6">
         <div class="flex flex-row justify-center items-center">
             <div class="flex flex-col space-y-4 w-96 m-8">
-                <h2 class="text-lg font-medium text-gray-900">MultiWorlds</h2>
+                <h2 class="flex items-center text-lg font-medium text-gray-900">
+                    <x-application-logo class="mr-2" />
+                    MultiWorlds
+                </h2>
                 <p class="mt-1 text-sm text-gray-600">
                     MultiWorlds is a mmo game where you can gather resources and make your own worlds.
                     The main focus of this game is to give players the tools to create their own things, and instantly share it with other players.
@@ -18,7 +21,10 @@
         <div class="flex flex-row justify-center items-center">
             <img class="w-[114] h-64 rounded shadow-md shadow-neutral-500" src="https://img.itch.zone/aW1hZ2UvMTU3MjA5Mi85MTgwMTY0LnBuZw==/original/7slLQa.png" />
             <div class="flex flex-col space-y-4 w-96 m-8">
-                <h2 class="text-lg font-medium text-gray-900">Current features</h2>
+                <h2 class="flex items-center text-lg font-medium text-gray-900">
+                    <img src="{{ URL::to('/') }}/media/pickaxe.png" class="mr-2" style="image-rendering: pixelated;" />
+                    Current features
+                </h2>
                 <ul class="list-disc text-sm text-gray-600 ml-4">
                     <li>Multiplayer</li>
                     <li>Platformer mechanics</li>
@@ -38,7 +44,10 @@
 
         <div class="flex flex-row justify-center items-center">
             <div class="flex flex-col space-y-4 w-96 m-8">
-                <h2 class="text-lg font-medium text-gray-900">Community</h2>
+                <h2 class="flex items-center text-lg font-medium text-gray-900">
+                    <img src="{{ URL::to('/') }}/media/sign.png" class="mr-2" style="image-rendering: pixelated;" />
+                    Community
+                </h2>
                 <p class="mt-1 text-sm text-gray-600">
                     This website has a forum where you can discuss things about the game or make suggestions.
                     If you are interested in our game you can also join our <a class="text-cyan-600 underline" href="https://discord.gg/AJvVWXTWKf">discord server</a> for more updates.
