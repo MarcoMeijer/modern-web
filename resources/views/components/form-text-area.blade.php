4e3a15b79d346b961bcff01fa0c1b62374614bcf
Marco Meijer <marcomeijerm@gmail.com>
2022-11-22 10:34:25 +0100

    chore: removed unused text area

--- resources/views/components/form-text-area.blade.php
+++
@@ -1,8 +0,0 @@
-@props(['name', 'label', 'placeholder', 'errors', 'value'])
-<div class="mt-2 mb-4">
-    <div class="flex flex-row">
-        <label class="w-24 text-gray-500 text-sm" for="{{$name}}">{{$label}}</label>
-        <textarea name="{{$name}}" placeholder="{{$placeholder}}" class="flex-1 border border-gray-400 rounded-sm p-1">{{ old($name, $value)  }}</textarea>
-    </div>
-    <x-form-error name="{{$name}}" :errors="$errors" />
-</div>
