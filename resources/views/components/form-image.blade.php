a74a79871c9dae13d3a29eb75b088f53c6400ea0
Marco Meijer <marcomeijerm@gmail.com>
2022-11-23 12:07:13 +0100

    fix: form image having right id and name

--- resources/views/components/form-image.blade.php
+++ resources/views/components/form-image.blade.php
@@ -1,7 +1,7 @@
 @props(['name', 'label', 'errors', 'value'])
 <div>
     <x-input-label :for="$name" :value="__($label)" />
-    <input type="file" :id="$name" :name="$name" accept="image/*" />
+    <input type="file" id="{{$name}}" name="{{$name}}" accept="image/*" />
     <img class="shadow-md h-32 w-32 rounded" src="{{$value}}" alt="">
     <x-input-error class="mt-2" :messages="$errors->get($name)" />
 </div>
